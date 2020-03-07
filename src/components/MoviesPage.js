091d66e998f6458bf3daf6718065a4ea63f5d6eb
Jordan McEachern <46895975+jordanMcE@users.noreply.github.com>
2020-03-06 21:28:43 -0400

    lazy loading implented successfully for main routes

---
+++ src/components/MoviesPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeoplePage = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '84px' }}>
+      [TODO: display movie content]
+    </div>
+  )
+}
+
+export default PeoplePage
---
+++ src/components/PeoplePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeoplePage = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '84px' }}>
+      [TODO: display people content]
+    </div>
+  )
+}
+
+export default PeoplePage
